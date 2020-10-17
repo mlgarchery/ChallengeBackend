c385b01673e879a4193b3d197e9a47bc24c75d64
mlgarchery <martial.garchery@gmail.com>
2020-10-17 23:18:51 +0200

    static files configuration for production

--- spotify_fetcher/spotify_fetcher/urls.py
+++ spotify_fetcher/spotify_fetcher/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 # Django
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
